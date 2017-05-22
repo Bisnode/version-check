18ee9b9c0cdcdf11e73b36143b30b51ac887babd
Martin Steiger <martin.steiger@bisnode.com>
2017-05-22 13:27:53 +0200

    First simple validation

---
+++ src/main/java/com/bisnode/versioncheck/ValidationTask.java
@@ -0,0 +1,175 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Simon Templer
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.bisnode.versioncheck;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.artifacts.ExternalDependency;
+import org.gradle.api.artifacts.ModuleVersionIdentifier;
+import org.gradle.api.artifacts.ResolvedArtifact;
+import org.gradle.api.artifacts.ResolvedConfiguration;
+import org.gradle.api.artifacts.ResolvedDependency;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.api.specs.Spec;
+import org.gradle.api.tasks.TaskAction;
+
+/**
+ * Validates the project dependencies against the defined ruleset.
+ */
+public class ValidationTask extends DefaultTask {
+
+    private static final Logger logger = Logging.getLogger(ValidationTask.class);
+
+    /**
+     * Accepts only external dependencies.
+     */
+    private static class ExternalDependencySpec implements Spec<Dependency> {
+        public boolean isSatisfiedBy(Dependency element) {
+            return element instanceof ExternalDependency;
+        }
+    }
+
+    private static final ExternalDependencySpec EXTERNAL_DEPENDENCY = new ExternalDependencySpec();
+
+    private final VersionCheckExtension extension = (VersionCheckExtension) getProject().getExtensions().getByName("versioncheck");
+
+
+    @TaskAction
+    public void validate() {
+
+        // determine projects for which to include dependencies
+        Set<Project> depProjects = determineProjects();
+
+        List<ModuleVersionIdentifier> allDeps = gatherDependencies(depProjects);
+
+        Map<String, String> versionGroup = new HashMap<>();
+
+        for (ModuleVersionIdentifier info : allDeps) {
+            String id = info.getGroup();
+            String version = versionGroup.get(id);
+            if (version == null) {
+                versionGroup.put(id, info.getVersion());
+            } else {
+                if (!version.equals(info.getVersion())) {
+                    logger.warn("Version mismatch for {}:{} vs. {}", id, version, info.getVersion());
+                }
+            }
+        }
+    }
+
+    private List<ModuleVersionIdentifier> gatherDependencies(Set<Project> depProjects) {
+        List<ModuleVersionIdentifier> allDeps = new ArrayList<>();
+
+        // project dependencies
+        for (Project depProject : depProjects) {
+            ConfigurationContainer configs = depProject.getConfigurations();
+            for (String name : configs.getNames()) {
+                // check if configuration should be included
+                if (extension.acceptConfiguration(name)) {
+                    Configuration cfg = configs.getByName(name);
+                    if (cfg.isCanBeResolved()) {
+                        ResolvedConfiguration config = cfg.getResolvedConfiguration();
+                         allDeps.addAll(getDependencies(name, config));
+                    }
+                }
+            }
+        }
+
+        // project plugins
+//    if (project.versioneye.includePlugins) {
+//      depProjects.each { Project depProject ->
+//        depProject.buildscript.configurations.names.collect { String name ->
+//          //XXX are there any build script configurations that should not be included?
+//          ResolvedConfiguration config = depProject.buildscript.configurations.getByName(name).resolvedConfiguration
+//          addDependenciesToMap(name, config, dependencyMap, 'plugin')
+//        }
+//      }
+//    }
+//
+
+        return allDeps;
+    }
+
+    private Set<Project> determineProjects() {
+        Project project = getProject();
+        if (extension.isIncludeSubProjects()) {
+            // project and sub-projects
+            return project.getAllprojects();
+        } else {
+            // only the main project
+            return Collections.singleton(project);
+        }
+    }
+
+    private List<ModuleVersionIdentifier> getDependencies(String configName, ResolvedConfiguration resolvedConfig) {
+        Set<?> deps;
+
+        Project project = getProject();
+        VersionCheckExtension versioncheckExt = (VersionCheckExtension) project.getExtensions()
+                .getByName("versioncheck");
+
+        if (VersionCheckExtension.transitive.equals(versioncheckExt.getDependencies())) {
+            // transitive dependencies: use all resolved artifacts
+
+            // retrieve all external dependencies
+            // XXX we are ignoring the dependency relations here - does it matter for VersionCheck?
+            deps = resolvedConfig.getLenientConfiguration().getArtifacts(EXTERNAL_DEPENDENCY);
+        } else if (VersionCheckExtension.declared.equals(versioncheckExt.getDependencies())) {
+            // declared dependencies: only use first level dependencies
+            deps = resolvedConfig.getLenientConfiguration().getFirstLevelModuleDependencies(EXTERNAL_DEPENDENCY);
+        } else {
+            logger.warn("Unknown dependency configuration - ignoring");
+            deps = Collections.emptySet();
+        }
+
+        List<ModuleVersionIdentifier> dependencyList = new ArrayList<>();
+        for (Object dependency : deps) {
+            if (dependency instanceof ResolvedArtifact) {
+                ResolvedArtifact artifact = (ResolvedArtifact) dependency;
+                ModuleVersionIdentifier id = artifact.getModuleVersion().getId();
+                dependencyList.add(id);
+            } else if (dependency instanceof ResolvedDependency) {
+                ResolvedDependency dep = (ResolvedDependency) dependency;
+                ModuleVersionIdentifier id = dep.getModule().getId();
+                dependencyList.add(id);
+            }
+        }
+        return dependencyList;
+    }
+
+}
--- src/main/java/com/bisnode/versioncheck/VersionCheckPlugin.java
+++ src/main/java/com/bisnode/versioncheck/VersionCheckPlugin.java
@@ -1,56 +1,33 @@
 
 package com.bisnode.versioncheck;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.io.File;
-import java.util.jar.*;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
-import org.gradle.api.artifacts.Configuration;
-import org.gradle.api.artifacts.ResolvedConfiguration;
-import org.gradle.api.artifacts.ResolvedArtifact;
 
 /**
- * VersionEye plugin for Gradle.
- *
- * @author Simon Templer
+ * VersionCheck plugin for Gradle.
  */
 public class VersionCheckPlugin implements Plugin<Project> {
 
+    @Override
+    public void apply(Project project) {
 
-  public static final String PROP_PROJECT_ID = "versioneye.projectid";
-  public static final String PROP_API_KEY = "versioneye.api_key";
+        // create extension
+        project.getExtensions().create("versioncheck", VersionCheckExtension.class, project);
 
-  public static final String PROP_ORGANISATION = "versioneye.organisation";
-  public static final String PROP_TEAM = "versioneye.team";
+        Map<String, Object> args = new HashMap<>();
+        args.put(Task.TASK_TYPE, ValidationTask.class);
+        Task jsonTask = project.task(args, "versionCheck");
 
-  private Project project;
-
-  private File dependenciesFile;
-
-  private File gradleProperties;
-
-  @Override
-  public void apply(Project project) {
-    this.project = project;
-
-    // create extension
-    project.getExtensions().create("versioncheck", VersionCheckExtension.class, project);
-
-    // task creating the POM representation of the dependencies
-    Task jsonTask = project.task("versionCheck");
-
-    jsonTask.setDescription("Generate a pom.json file with the project\"s dependencies");
-    jsonTask.setGroup("VersionCheck");
-    jsonTask.doLast((task) -> {
-        VersionCheckExtension ext = (VersionCheckExtension) project.getExtensions().getByName("versioncheck");
-        System.out.println("TASK: " + ext.getClass().getSimpleName());
-    });
-//    createTask.dependsOn(jsonTask)
-  }
-
+        jsonTask.setDescription("Generate a pom.json file with the project\"s dependencies");
+        jsonTask.setGroup("VersionCheck");
+        jsonTask.doLast((task) -> {
+            VersionCheckExtension ext = (VersionCheckExtension) project.getExtensions().getByName("versioncheck");
+        });
+    }
 }
+
